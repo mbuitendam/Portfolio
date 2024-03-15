a00418f06a2f572194c00de90b67b5f3f88576c1
mbuitendam <149648435+mbuitendam@users.noreply.github.com>
2024-03-15 11:45:54 +1100

    Add files via upload

--- ToDoList/forms.py
+++ ToDoList/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField, DateField
+from wtforms import StringField, SubmitField, PasswordField, EmailField, DateField, SelectField
 from wtforms.validators import DataRequired
 from flask_ckeditor import CKEditorField
 
@@ -7,8 +7,9 @@ from flask_ckeditor import CKEditorField
 class RegisterUser(FlaskForm):
     email = EmailField("Email", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Your Name", validators=[DataRequired()])
-    role = StringField("What is your role", validators=[DataRequired()])
+    firstName = StringField("First Name", validators=[DataRequired()])
+    lastName = StringField("First Name", validators=[DataRequired()])
+    role = SelectField("Priority Level", choices=[("admin", "Admin"), ("manager", "Manager"), ("employee", "Employee")], validators=[DataRequired()])
     submit = SubmitField("Register")
 
 
@@ -23,5 +24,6 @@ class addToDo(FlaskForm):
     title = StringField("Title", validators=[DataRequired()])
     subheading = StringField("Subheading", validators=[DataRequired()])
     content = CKEditorField("What are you trying to do", validators=[DataRequired()])
-    dueDate = DateField()
+    dueDate = DateField(validators=[DataRequired()])
+    priority = SelectField("Priority Level", choices=[("urgent", "Urgent"), ("high", "High"), ("medium", "Medium"), ("low", "Low")], validators=[DataRequired()])
     submit = SubmitField("Add Item")
